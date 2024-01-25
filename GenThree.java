2a39e88c4f9028d7a2d51aea92759a4d39d6362e
Yuval Baron <yuval.baron@post.runi.ac.il>
2024-01-25 16:43:43 +0200

    Add awesome code to Homework 1

--- GenThree.java
+++ GenThree.java
@@ -6,9 +6,13 @@ public class GenThree {
 	public static void main(String[] args) {
 		int min = Integer.parseInt(args[0]);
 		int max = Integer.parseInt(args[1]);
-		System.out.println((int)((Math.random()*(max - min + 1)) + min));
-		System.out.println((int)((Math.random()*(max - min + 1)) + min));
-		System.out.println((int)((Math.random()*(max - min + 1)) + min));
-		System.out.println("The minimal generated number was " + min);
+		int a = (int)((Math.random()*(max - min + 1)) + min);
+		int b = (int)((Math.random()*(max - min + 1)) + min);
+		int c = (int)((Math.random()*(max - min + 1)) + min);
+		int minGen = Math.min(Math.min(a, b), c);
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println("The minimal generated number was " + minGen);
 	}
 }
--- Triangle.java
+++ Triangle.java
@@ -9,7 +9,7 @@ public class Triangle {
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
 		boolean isTriangle = ((a + b > c) && (b + c > a) && (c + a > b));
-			System.out.println((a + " ," + b + " ," + c + ": ") + (isTriangle));
+			System.out.println((a + ", " + b + ", " + c + ": ") + (isTriangle));
 			
 	}
 }
